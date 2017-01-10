d4776a06b2602ce697904f836ef240567f9990ce
穆康 <mukang@buluo-gs.com>
2017-01-10 12:11:04 +0800

    [新增] 新建项目：store

---
+++ store/store/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  store
+//
+//  Created by 穆康 on 2017/1/10.
+//  Copyright © 2017年 杭州部落公社科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ store/store/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  store
+//
+//  Created by 穆康 on 2017/1/10.
+//  Copyright © 2017年 杭州部落公社科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
